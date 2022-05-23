6fd1a891b00271f56fce3d3fb8c95a7be35dbbb0
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2022-05-23 18:04:17 +0300

    Update sei.sh

--- sei.sh
+++ sei.sh
@@ -25,10 +25,10 @@ echo "export WALLET=wallet" >> $HOME/.bash_profile
 echo "export CHAIN_ID=sei-testnet-1" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
-sleep 2
 
 # update
 sudo apt update && sudo apt upgrade -y
+
 
 # packages
 sudo apt install curl tar wget clang pkg-config libssl-dev jq build-essential bsdmainutils git make ncdu gcc git jq chrony liblz4-tool -y
@@ -43,6 +43,7 @@ rm "go$ver.linux-amd64.tar.gz"
 echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
 source ~/.bash_profile
 go version
+
 
 # download binary
 cd $HOME
@@ -60,15 +61,15 @@ seid config keyring-backend file
 seid init $NODENAME --chain-id $CHAIN_ID
 
 # download genesis and addrbook
-wget $HOME/.sei-chain/config/genesis.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/genesis.json"
-wget $HOME/.sei-chain/config/addrbook.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/addrbook.json"
+wget -qO $HOME/.sei-chain/config/genesis.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/genesis.json"
+wget -qO $HOME/.sei-chain/config/addrbook.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/addrbook.json"
 
 # set minimum gas price
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0usei\"/" $HOME/.sei-chain/config/app.toml
 
 # set peers and seeds
 SEEDS=""
-PEERS="5082637d2face9dd32c4ad7eff34d38df4244c9a@65.21.123.69:26642"
+PEERS="5082637d2face9dd32c4ad7eff34d38df4244c9a@65.21.123.69:26642,2f047e234cb8b99fe8b9fee0059a5bc45042bc97@95.216.84.188:26656,38b4d78c7d6582fb170f6c19330a7e37e6964212@194.163.189.114:46656"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.sei-chain/config/config.toml
 
 # enable prometheus
@@ -86,7 +87,8 @@ sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.sei-chain/config/app.toml
 
 # reset
-seid unsafe-reset-all
+seid tendermint unsafe-reset-all
+
 
 # create service
 tee $HOME/seid.service > /dev/null <<EOF
