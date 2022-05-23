b930064f9b4afaf34092aba7e7603b893d7ec877
Uncle Bogdan <85982863+freshe4qa@users.noreply.github.com>
2022-05-23 15:31:49 +0300

    Update sei.sh

--- sei.sh
+++ sei.sh
@@ -60,8 +60,8 @@ seid config keyring-backend file
 seid init $NODENAME --chain-id $CHAIN_ID
 
 # download genesis and addrbook
-wget -qO $HOME/.sei-chain/config/genesis.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/genesis.json"
-wget -qO $HOME/.sei-chain/config/addrbook.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/addrbook.json"
+wget $HOME/.sei-chain/config/genesis.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/genesis.json"
+wget $HOME/.sei-chain/config/addrbook.json "https://raw.githubusercontent.com/sei-protocol/testnet/master/sei-testnet-1/addrbook.json"
 
 # set minimum gas price
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0usei\"/" $HOME/.sei-chain/config/app.toml
